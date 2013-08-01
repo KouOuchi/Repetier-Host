369abecc870b95e2d042a7f17e0e2cdcbb404ca2
KouOuchi <kou.ouchi@division-engineering.com>
2013-08-01 17:31:09 +0900

    Once showing label is removed.

--- src/RepetierHost/model/Coordinate.cs
+++ src/RepetierHost/model/Coordinate.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using OpenTK;
 using OpenTK.Graphics.OpenGL;
-using QuickFont;
 using RepetierHost.model.geom;
 
 namespace RepetierHost.model
@@ -10,8 +9,7 @@ namespace RepetierHost.model
     {
         #region defines
         const int area_size = 80;
-        const int margin = 16;
-        const int label_font_size = 16;
+        const int margin = 4;
         readonly Vector4 z_unit = new Vector4(0.0f, 0.0f, area_size, 0.0f);
         readonly int[] coordinate_colors = 
         {
@@ -29,7 +27,6 @@ namespace RepetierHost.model
 
         Submesh[] submesh = null;
         TopoModel model = null;
-        QFont font;
         float arrow_length;
 
         internal Coordinate()
@@ -49,11 +46,6 @@ namespace RepetierHost.model
                 submesh[i].Compress(true, coordinate_colors[i]);
                 submesh[i].vertices.Clear();
             }
-
-            // config font
-            font = new QFont("data/HappySans.ttf",
-                label_font_size, new QFontBuilderConfiguration(true));
-            font.Options.DropShadowActive = false;
         }
 
         internal void Draw(int viewport_x, int viewport_y, double rotX, double rotZ)
@@ -74,7 +66,6 @@ namespace RepetierHost.model
             GL.Rotate(rotZ / Math.PI * 180D, 0D, 0D, -1D);
 
             DrawArrow();
-            DrawLabel();
 
             GL.Viewport(0, 0, viewport_x, viewport_y); // restore viewport
 
@@ -98,43 +89,6 @@ namespace RepetierHost.model
             GL.Rotate(90.0d, 1.0d, 0.0d, 0.0d);
             submesh[0].Draw(Main.threeDSettings.drawMethod, Main.main.threedview.cam.EdgeTranslation());
             GL.PopMatrix();
-        }
-
-        private void DrawLabel()
-        {
-            Matrix4 movelview;
-            GL.GetFloat(GetPName.ModelviewMatrix, out movelview);
-
-            float len = area_size / 2.0f * arrow_length;
-            Vector4 pos_x = Vector4.Transform(-Vector4.UnitY * len, movelview);
-            Vector4 pos_y = Vector4.Transform(Vector4.UnitX * len, movelview);
-            Vector4 pos_z = Vector4.Transform(Vector4.UnitZ * len, movelview);
-
-            QFont.Begin();
-            GL.PushMatrix();
-            GL.Disable(EnableCap.DepthTest);
-            GL.Disable(EnableCap.Lighting);
-            // translate to center
-            GL.Translate(area_size * 0.5f, area_size * 0.5f - label_font_size, 0f);
-
-            Vector2 translate_x = new Vector2(pos_x.X, -pos_x.Y - label_font_size / 2.0f);
-            Vector2 translate_y = new Vector2(pos_y.X, -pos_y.Y - label_font_size / 2.0f);
-            Vector2 translate_z = new Vector2(pos_z.X, -pos_z.Y - label_font_size / 2.0f);
-
-            font.Options.Colour = label_colors[0];
-            font.Print("x", QFontAlignment.Left, translate_x);
-            font.Options.Colour = label_colors[1];
-            font.Print("y", QFontAlignment.Right, translate_y);
-            font.Options.Colour = label_colors[2];
-            font.Print("z", QFontAlignment.Left, translate_z);
-
-            GL.Disable(EnableCap.Texture2D); // QFont doesn't restor Texture2D. 
-            GL.Disable(EnableCap.Blend); // QFont doesn't restore Blend.
-
-            GL.Enable(EnableCap.Lighting);
-            GL.Enable(EnableCap.DepthTest);
-            GL.PopMatrix();
-            QFont.End();
         }
     }
 }
